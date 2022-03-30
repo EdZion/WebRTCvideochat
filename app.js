f7cf16801b9c86fa76d304e68c9b06d123b95760
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2022-03-30 13:45:08 +0200

    fixed html compatibility

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', indexRouter);
 
